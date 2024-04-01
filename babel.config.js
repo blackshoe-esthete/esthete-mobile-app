b56d3d60a6305f0c125f052de3c6afb6134380c3
Kong-E <kng001016@dgu.ac.kr>
2024-04-02 01:13:28 +0900

    Chore: 절대경로 설정

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,22 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          src: './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@assets': './src/assets',
+          '@utils': './src/utils',
+          '@constants': './src/constants',
+          '@hooks': './src/hooks',
+          '@navigations': './src/navigations',
+          '@types': './src/types',
+          '@apis': './src/apis',
+        },
+      },
+    ],
+  ],
 };
