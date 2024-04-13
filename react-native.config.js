f63793e33bbf1605b32ece07ecd0aae602de737f
g2Min <jimin9320@gmail.com>
2024-04-13 21:08:09 +0900

    fix: 환경설정

---
+++ react-native.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    dependencies: {
+      // Conditionally exclude 'react-native-flipper' for iOS builds if NO_FLIPPER is set
+      ...(process.env.NO_FLIPPER === '1' ? { 'react-native-flipper': { platforms: { ios: null } } } : {})
+    }
+  };
+  
