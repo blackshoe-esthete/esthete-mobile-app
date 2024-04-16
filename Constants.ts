55191cb50567e189d726034734649b5d6e13d47c
g2Min <jimin9320@gmail.com>
2024-04-17 04:45:31 +0900

    feat: 상수 설정

--- Constants.ts
+++ Constants.ts
@@ -1,8 +1,7 @@
-import { Dimensions, Platform } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import StaticSafeAreaInsets from 'react-native-static-safe-area-insets'
+import { Dimensions, Platform } from 'react-native';
+import StaticSafeAreaInsets from 'react-native-static-safe-area-insets';
 
-export const CONTENT_SPACING = 15
+export const CONTENT_SPACING = 15;
 
 const SAFE_BOTTOM =
   Platform.select({
@@ -17,16 +16,16 @@ export const SAFE_AREA_PADDING = {
 };
 
 // The maximum zoom _factor_ you should be able to zoom in
-export const MAX_ZOOM_FACTOR = 10
+export const MAX_ZOOM_FACTOR = 10;
 
-export const SCREEN_WIDTH = Dimensions.get('window').width
+export const SCREEN_WIDTH = Dimensions.get('window').width;
 export const SCREEN_HEIGHT = Platform.select<number>({
   android: Dimensions.get('screen').height - StaticSafeAreaInsets.safeAreaInsetsBottom,
   ios: Dimensions.get('window').height,
-}) as number
+}) as number;
 
 // Capture Button
-export const CAPTURE_BUTTON_SIZE = 78
+export const CAPTURE_BUTTON_SIZE = 78;
 
 // Control Button like Flash
-export const CONTROL_BUTTON_SIZE = 40
+export const CONTROL_BUTTON_SIZE = 40;
