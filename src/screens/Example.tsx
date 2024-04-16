de754edf9aeef65748415fa555c08d47264335c2
g2Min <jimin9320@gmail.com>
2024-04-17 02:44:32 +0900

    test: 안드로이드 성공

--- src/screens/Example.tsx
+++ src/screens/Example.tsx
@@ -1,18 +1,62 @@
-import { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { useEffect, useRef, useState, useCallback } from 'react';
+import { View, Text, StyleSheet, PermissionsAndroid, ActivityIndicator } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { useCameraPermission, useCameraDevice, Camera } from 'react-native-vision-camera';
+import { useCameraPermission, useCameraDevices, Camera, CameraPermissionStatus, CameraRuntimeError, useCameraDevice } from 'react-native-vision-camera';
+
+const requestCameraPermission = async () => {
+  try {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.CAMERA,
+      {
+        title: "Sample Photo App Camera Permission",
+        message:
+        "Sample Photo App needs access to your camera " +
+        "so you can take awesome pictures.",
+        buttonNeutral: "Ask Me Later",
+        buttonNegative: "Cancel",
+        buttonPositive: "OK"
+      }
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log("You can use the camera");
+    } else {
+      console.log("Camera permission denied");
+    }
+  } catch (err) {
+    console.warn(err);
+  }
+};
 
 export function Example(): React.JSX.Element {
-  const device = useCameraDevice('back');
+  console.log("App Started");
+
+  useEffect(() => {
+    requestCameraPermission();
+  }, []);
+  
+  const device = useCameraDevice('front');
+
+  if (device == null){ 
+    return <Text>Loading</Text>
+  }else{
+    return (
+      <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={true}>
+      </Camera>
+    )
+  }
+}
+
+/*
+const device = useCameraDevice('back');
   const {hasPermission, requestPermission} = useCameraPermission();
   useEffect(() => {
     if(!hasPermission){
       requestPermission();
     }
   }, [hasPermission]);
-
-  console.log(hasPermission);
 
   if(!device){
     return <Text>카메라 없음</Text>
@@ -27,4 +71,5 @@ export function Example(): React.JSX.Element {
       />
     </SafeAreaView>
   );
-}
+
+*/
