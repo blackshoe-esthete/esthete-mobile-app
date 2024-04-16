aaec48de8b50a21521383fa798049560691319a2
g2Min <jimin9320@gmail.com>
2024-04-17 04:44:13 +0900

    feat: masonry list 이용한 필터 검색 렌더링

---
+++ src/components/FilterSearchScreen/RenderItem.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+import {Image, View, StyleSheet, Text} from 'react-native';
+import unlike from '@assets/icons/unlike.png';
+import like from '@assets/icons/like.png';
+import profile from '@assets/imgs/anonymous.png';
+
+type ImageItem = {
+  id: string;
+  source: {uri: string};
+  author?: string;
+  name?: string;
+  like?: boolean;
+  likeNum?: string;
+};
+
+function RenderItem({item}: {item: ImageItem; index: number}) {
+  return (
+    <View style={{flex: 1}}>
+      <View style={styles.likeBox}>
+        {item.like ? (
+          <Image source={like} style={styles.icon} />
+        ) : (
+          <Image source={unlike} style={styles.icon} />
+        )}
+        <Text style={styles.num}>{item.likeNum}</Text>
+      </View>
+      <Image source={item.source} style={styles.image} />
+      <View style={styles.textBox}>
+        <Text style={styles.filterName}>{item.name}</Text>
+        <View style={styles.textInText}>
+          <Image source={profile} style={styles.profileSize} />
+          <Text style={styles.authorName}>{item.author}</Text>
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  image: {
+    margin: 10,
+    resizeMode: 'cover',
+  },
+  likeBox: {
+    position: 'absolute',
+    top: 15,
+    right: 5,
+    zIndex: 2,
+    alignItems: 'center',
+    gap: 3,
+  },
+  icon: {
+    width: 18,
+    height: 15,
+  },
+  num: {
+    fontSize: 10,
+    color: '#FFF',
+  },
+  textBox: {
+    height: 38,
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    paddingRight: 40,
+    marginHorizontal: 10,
+    // backgroundColor: "white"
+  },
+  textInText:{
+    display: 'flex',
+    width: '100%',
+    // backgroundColor: "white",
+    flexDirection: 'row',
+    justifyContent: 'flex-end'
+  },
+  profileSize: {
+    width: 20,
+    height: 20,
+    marginRight: 7
+  },
+  filterName: {
+    fontSize: 16,
+    color: '#fff',
+    fontWeight: '700'
+  },
+  authorName: {
+    fontSize: 12,
+    color: '#fff',
+    fontWeight: '500'
+  },
+});
+
+export default RenderItem;
