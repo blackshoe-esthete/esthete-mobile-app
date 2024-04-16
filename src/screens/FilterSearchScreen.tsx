e4481c08f134f90d91e7e2cffac5364166891f88
g2Min <jimin9320@gmail.com>
2024-04-16 11:48:18 +0900

    feat: backspace 아이콘 추가

--- src/screens/FilterSearchScreen.tsx
+++ src/screens/FilterSearchScreen.tsx
@@ -1,13 +1,15 @@
 import {useSafeAreaInsets, SafeAreaView} from 'react-native-safe-area-context';
 import React from 'react';
-import {ScrollView, StyleSheet, View, Image} from 'react-native';
+import {ScrollView, StyleSheet, View, Image, TouchableOpacity} from 'react-native';
 // import KeywordList from '@components/FilterSearchScreen/KeywordList';
 import KeywordList from '@components/FilterSearchScreen/KeywordList';
 import ExhibitionList from '@components/HomeScreen/ExhibitionList';
 import SearchBar from '@components/common/SearchBar';
-import back from '@assets/icons/backspace.png';
+import back from '@assets/icons/backspace_white.png';
+import { useNavigation } from '@react-navigation/native';
 
 function FilterSearchScreen(): React.JSX.Element {
+  const navigation = useNavigation();
   const {top} = useSafeAreaInsets();
 
   return (
@@ -17,10 +19,15 @@ function FilterSearchScreen(): React.JSX.Element {
         showsVerticalScrollIndicator={false}
         style={styles.scrollContainer}>
         {/* 검색창 */}
-        <View>
-          {/* <Image source={back}/> */}
-          <SearchBar left={30} label="필터, 작가 검색"/>
+        <View style={{display: 'flex', justifyContent: 'center'}}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={[styles.wrapper, {top: top + 21}]}>
+            <Image source={back} style={styles.icon} />
+          </TouchableOpacity>
+          <SearchBar left={40} label="필터, 작가 검색" width={320}/>
         </View>
+          
         {/* 키워드 */}
         <KeywordList />
         {/* 전시 목록 */}
@@ -40,7 +47,19 @@ const styles = StyleSheet.create({
   },
   searchBox: {
     paddingLeft: 30
-  }
+  },
+  icon: {
+    width: 8.5,
+    height: 20,
+    zIndex: 2,
+  },
+  wrapper: {
+    ...StyleSheet.absoluteFillObject,
+    left: 20,
+    width: 20,
+    height: 20,
+    zIndex: 2,
+  },
 });
 
 export default FilterSearchScreen;
