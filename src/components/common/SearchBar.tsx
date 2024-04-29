84028bebc4ad99ec3065a9c0992daec08aa8fcb6
g2Min <jimin9320@gmail.com>
2024-04-29 15:36:50 +0900

    [EST-68] Feat: 커스텀 검색바 구현

--- src/components/common/SearchBar.tsx
+++ src/components/common/SearchBar.tsx
@@ -8,50 +8,74 @@ import {
   View,
 } from 'react-native';
 import {useNavigation} from '@react-navigation/native';
+import backIcon from '@assets/icons/backspace_white.png';
 
 type SearchBarProps = {
   iconSource: File | Blob | string;
   to: string;
-  left?: number;
-  label?: string;
-  width?: number;
+  back?: boolean;
 };
 
-function SearchBar({
-  iconSource,
-  to,
-  left,
-  label,
-  width,
-}: PropsWithChildren<SearchBarProps>): React.JSX.Element {
+function SearchBar({iconSource, to, back}: SearchBarProps): React.JSX.Element {
   const navigation = useNavigation();
-  const textLeft: TextStyle | undefined = left ? {marginLeft: left} : undefined;
-  const boxWidth: TextStyle | undefined = width ? {width: width} : undefined;
-  console.log(label);
-  return (
-    <View style={[styles.textInput, textLeft, boxWidth]}>
-      <TextInput
-        editable
-        placeholder={label ? label : '전시회, 작가 검색'}
-        placeholderTextColor="#DADADA"
-        style={[styles.text, {fontWeight: '400'}]}
-      />
-      <TouchableOpacity onPress={() => navigation.navigate(to)}>
-        <Image
-          source={iconSource}
-          style={{
-            width: 18,
-            height: 18,
-          }}
+  if (back) {
+    return (
+      <View style={styles.wrapper}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Image source={backIcon} style={styles.icon} />
+        </TouchableOpacity>
+        <View style={[styles.textInput, {marginTop: 0, width: '95%'}]}>
+          <TextInput
+            editable
+            placeholder="전시회, 작가 검색"
+            placeholderTextColor="#DADADA"
+            style={[styles.text, {fontWeight: '400'}]}
+          />
+          <TouchableOpacity onPress={() => navigation.navigate(to)}>
+            <Image
+              source={iconSource}
+              style={{
+                width: 18,
+                height: 18,
+              }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.textInput}>
+        <TextInput
+          editable
+          placeholder="전시회, 작가 검색"
+          placeholderTextColor="#DADADA"
+          style={[styles.text, {fontWeight: '400'}]}
         />
-      </TouchableOpacity>
-    </View>
-  );
+        <TouchableOpacity onPress={() => navigation.navigate(to)}>
+          <Image
+            source={iconSource}
+            style={{
+              width: 18,
+              height: 18,
+            }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 export default SearchBar;
 
 const styles = StyleSheet.create({
+  wrapper: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginTop: 30,
+    width: '100%',
+    paddingHorizontal: 20,
+  },
   textInput: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -69,5 +93,10 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     padding: 0,
     margin: 0,
+  },
+  icon: {
+    width: 8.5,
+    height: 20,
+    // marginLeft: 20,
   },
 });
