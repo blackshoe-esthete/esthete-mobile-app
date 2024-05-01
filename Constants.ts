393f9d3e8aebac239793947cd2391447a6765b50
g2Min <jimin9320@gmail.com>
2024-05-01 19:46:37 +0900

    [EST-68] Feat:  필요한 상수값

--- Constants.ts
+++ Constants.ts
@@ -10,7 +10,7 @@ const SAFE_BOTTOM =
 
 export const SAFE_AREA_PADDING = {
   paddingLeft: StaticSafeAreaInsets.safeAreaInsetsLeft + CONTENT_SPACING,
-  paddingTop: StaticSafeAreaInsets.safeAreaInsetsTop + CONTENT_SPACING,
+  // paddingTop: StaticSafeAreaInsets.safeAreaInsetsTop + CONTENT_SPACING,
   paddingRight: StaticSafeAreaInsets.safeAreaInsetsRight + CONTENT_SPACING,
   paddingBottom: SAFE_BOTTOM + CONTENT_SPACING,
 };
