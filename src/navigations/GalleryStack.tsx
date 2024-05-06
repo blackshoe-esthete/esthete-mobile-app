e59ce8f602ac2660b64030d83807b18f2d1c063e
g2Min <jimin9320@gmail.com>
2024-05-07 03:39:36 +0900

    [EST-43] Feat: my menu nav바 정리

--- src/navigations/GalleryStack.tsx
+++ src/navigations/GalleryStack.tsx
@@ -1,24 +1,54 @@
-import React from "react";
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
-import MyGalleryScreen from "@components/MyGalleryScreen/MyHeader";
-import MyTab from "./MyTab";
+import React from 'react';
+import {
+  createNativeStackNavigator,
+  NativeStackScreenProps,
+} from '@react-navigation/native-stack';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {Routes} from '@screens/Routes';
+import MyTab from './MyTab';
+import MyMenu from '@screens/MyMenu/MyMenu';
+import MyLikes from '@screens/MyMenu/MyLikeScreen';
+import Temporary from '@screens/MyMenu/TemporaryScreen';
+import SubScribe from '@screens/MyMenu/SubscribeScreen';
+import Settings from '@screens/MyMenu/SettingScreen';
+import Information from '@screens/MyMenu/InformationScreen';
+import Logout from '@screens/MyMenu/LogoutScreen';
 
 const Stack = createNativeStackNavigator();
 
-function GalleryStack(): React.JSX.Element{
-  return(
+type Props = NativeStackScreenProps<Routes, 'MyTab'>;
+function GalleryStack({navigation, route}: Props): React.JSX.Element {
+  return (
     <SafeAreaProvider>
-      <Stack.Navigator>
-        <Stack.Screen 
-        name="MyTab"
-        options={{
-          headerShown: false
-        }}
-        >
-          {(props: any) => <MyTab {...props}/>}
+      <Stack.Navigator
+        initialRouteName="MyTab"
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="MyTab">
+          {(props: any) => <MyTab {...props} />}
         </Stack.Screen>
-
+        <Stack.Screen name="MyMenu">
+          {(props: any) => <MyMenu {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="MyLikes">
+          {(props: any) => <MyLikes {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="Temporary">
+          {(props: any) => <Temporary {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="SubScribe">
+          {(props: any) => <SubScribe {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="Settings">
+          {(props: any) => <Settings {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="Information">
+          {(props: any) => <Information {...props} />}
+        </Stack.Screen>
+        <Stack.Screen name="Logout">
+          {(props: any) => <Logout {...props} />}
+        </Stack.Screen>
       </Stack.Navigator>
     </SafeAreaProvider>
   );
