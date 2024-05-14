aac223d9688e3bf95220c9fb2b9aaaf01979bd27
g2Min <jimin9320@gmail.com>
2024-05-15 03:11:29 +0900

    [EST-43] Feat: 선호태그 편집

--- src/components/SettingScreen/Preferred.tsx
+++ src/components/SettingScreen/Preferred.tsx
@@ -1,55 +1,107 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {View, StyleSheet, Text, TouchableOpacity, Image} from 'react-native';
+import  cancel from '@assets/icons/cancel_gray.png';
 
 function Preferred() {
-  const tags: string[] = [
-    '자연스러운',
-    '클래식한',
-    '쾌활한',
-    '세련된',
-    '우아한',
-    '잔잔한',
-    '풍요로운',
-    '차가운',
-    '평화로운',
-    '짙은',
-    '빈티지한',
-    '화사한',
-    '모던한',
-    '수수한',
-    '부드러운',
-    '사랑스러운',
-    '캐쥬얼한',
-    '자유분방한',
-    '다채로운',
-    '차분한',
-    '신비로운',
-    '순수한',
-    '고요한',
-    '고급스러운',
-  ];
-
-  type tag = {
+  type tagForm = {
+    id: number;
     title: string;
   };
-  const TagForm = (prop: tag) => {
+  const tags: tagForm[] = [
+    {id: 0, title: '자연스러운'},
+    {id: 1, title: '클래식한'},
+    {id: 2, title: '쾌활한'},
+    {id: 3, title: '세련된'},
+    {id: 4, title: '우아한'},
+    {id: 5, title: '잔잔한'},
+    {id: 6, title: '풍요로운'},
+    {id: 7, title: '차가운'},
+    {id: 8, title: '평화로운'},
+    {id: 9, title: '짙은'},
+    {id: 10, title: '빈티지한'},
+    {id: 11, title: '화사한'},
+    {id: 12, title: '모던한'},
+    {id: 13, title: '수수한'},
+    {id: 14, title: '부드러운'},
+    {id: 15, title: '사랑스러운'},
+    {id: 16, title: '캐쥬얼한'},
+    {id: 17, title: '자유분방한'},
+    {id: 18, title: '다채로운'},
+    {id: 19, title: '차분한'},
+    {id: 20, title: '신비로운'},
+    {id: 21, title: '순수한'},
+    {id: 22, title: '고요한'},
+    {id: 23, title: '고급스러운'},
+  ];
+
+  const initial = Array(tags.length).fill(false);
+  const [status, setStatus] = useState<boolean[]>(initial);
+  const [filter, setFilter] = useState<tagForm[]>([]);
+
+  const TagForm = (prop: tagForm) => {
+    const {id, title} = prop;
     return (
-      <View style={styles.tagBox}>
-        <Text style={styles.tagTitle}>{prop.title}</Text>
+      <TouchableOpacity
+        onPress={() => {
+          const newStatus = [...status];
+          newStatus[id] = !newStatus[id];
+          setStatus(newStatus);
+        }}>
+        {status[id] ? (
+          <View style={styles.focusedTagBox}>
+            <Text style={styles.focusedTagTitle}>{title}</Text>
+          </View>
+        ) : (
+          <View style={styles.tagBox}>
+            <Text style={styles.tagTitle}>{title}</Text>
+          </View>
+        )}
+      </TouchableOpacity>
+    );
+  };
+
+  const SelectedTag = (prop: tagForm) => {
+    const {id, title} = prop;
+    return (
+      <View style={styles.selectedTag}>
+        <Text style={styles.tagTitle}>{title}</Text>
+        <TouchableOpacity onPress={() => {
+          const newStatus = [...status];
+          newStatus[id] = !newStatus[id];
+          setStatus(newStatus);
+        }}>
+          <Image source={cancel} style={styles.icon}/>
+        </TouchableOpacity>
       </View>
     );
   };
+
+  useEffect(()=>{
+    const newFilteredTags = tags.filter((item) => status[item.id] === true);
+    setFilter(newFilteredTags);
+  }, [status]);
 
   return (
     <View style={styles.container}>
       <Text style={styles.intro}>
         선호하는 태그를 10개 선택해주세요 (최대 10개)
       </Text>
-      {tags.map((item, index) => 
-        <View>
-          {/* {(item: any) => <TagForm {...item}/>} */}
+      <View style={styles.tagContainer}>
+        {tags.map((item, index) => (
+          <TagForm key={index} id={item.id} title={item.title} />
+        ))}
+      </View>
+      <Text style={[styles.intro, {marginTop: 50}]}>내가 선택한 태그</Text>
+      <View style={styles.tagContainer}>
+        {filter && filter.map((item, index) => (
+          <SelectedTag key={index} id={item.id} title={item.title} />
+        ))}
+      </View>
+      <TouchableOpacity style={{marginTop: 160}}>
+        <View style={styles.button}>
+          <Text style={styles.buttonTitle}>저장하기</Text>
         </View>
-      )}
+      </TouchableOpacity>
     </View>
   );
 }
@@ -60,6 +112,7 @@ const styles = StyleSheet.create({
   container: {
     padding: 20,
     width: '100%',
+    marginBottom: 60
   },
   intro: {
     color: 'white',
@@ -73,11 +126,65 @@ const styles = StyleSheet.create({
     padding: 15,
     display: 'flex',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    width: 'auto',
+  },
+  focusedTagBox: {
+    borderRadius: 10,
+    backgroundColor: '#FFD600',
+    padding: 15,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 'auto',
   },
   tagTitle: {
     color: '#F4F4F4',
     fontSize: 16,
-    fontWeight: '500'
+    fontWeight: '500',
+  },
+  focusedTagTitle: {
+    color: 'black',
+    fontSize: 16,
+    fontWeight: '500',
+  },
+  tagContainer: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    gap: 10,
+    marginTop: 20,
+  },
+  selectedTag: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    // paddingHorizontal: 15,
+    paddingLeft: 15,
+    paddingRight: 11,
+    backgroundColor: '#414141',
+    padding: 15,
+    borderRadius: 10,
+    gap: 5
+  },
+  icon: {
+    width: 18,
+    height: 18,
+  },
+  button: {
+    backgroundColor: '#FFD600',
+    borderRadius: 10,
+    width: '100%',
+    height: 60,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonTitle: {
+    color: '#030303',
+    fontSize: 18,
+    letterSpacing: -0.36,
+    fontWeight: '700'
   }
 });
--- src/screens/MyMenu/PreferEdit.tsx
+++ src/screens/MyMenu/PreferEdit.tsx
@@ -1,15 +1,27 @@
-import MenuHeader from "@components/MyMenuScreen/MenuHeader";
-import Preferred from "@components/SettingScreen/Preferred";
-import React from "react";
-import { SafeAreaView } from "react-native-safe-area-context";
+import MenuHeader from '@components/MyMenuScreen/MenuHeader';
+import Preferred from '@components/SettingScreen/Preferred';
+import React from 'react';
+import {ScrollView, StyleSheet} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
-function PreferEdit(){
-  return(
+function PreferEdit() {
+  return (
     <SafeAreaView edges={['top']}>
-      <MenuHeader title="선호 태그 편집"/>
-      <Preferred />
+      <MenuHeader title="선호 태그 편집" />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        style={styles.scrollContainer}>
+        <Preferred />
+      </ScrollView>
     </SafeAreaView>
-  )
+  );
 }
 
 export default PreferEdit;
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    width: '100%',
+    backgroundColor: '#030303',
+  },
+});
