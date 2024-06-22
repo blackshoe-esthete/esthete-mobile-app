07f7a0431e21de7633514899f9117407607cf8e2
102sae <102chimee@gmail.com>
2024-06-22 15:41:04 +0900

    [EST-36] Add : QueryClient 추가

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 // import type {PropsWithChildren} from 'react';
-
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
@@ -21,6 +21,7 @@ import ExhibitionFilterApplyCompleteScreen from './src/screens/ExhibitionCreatio
 import PlacesSearchScreen from './src/screens/ExhibitionCreation/PlacesSearchScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
+const queryClient = new QueryClient();
 
 const MyTheme = {
   ...DefaultTheme,
@@ -34,54 +35,60 @@ function App(): React.JSX.Element {
   // IOS에서는 상태바 색상을 현재 화면의 배경색과 동일하게 하려면 이 방법을 써야함
   return (
     <SafeAreaProvider>
-      <NavigationContainer theme={MyTheme}>
-        <Stack.Navigator>
-          <Stack.Group>
-            <Stack.Screen name="Main" component={BottomTab} options={{headerShown: false}} />
-            <Stack.Screen name="Map" component={MapScreen} options={{headerShown: false}} />
-            <Stack.Screen name="HomeSearch" component={HomeSearchScreen} options={{headerShown: false}} />
-          </Stack.Group>
-          <Stack.Group>
-            <Stack.Screen name="Exhibition" component={ExhibitionScreen} options={{headerShown: false}} />
-            <Stack.Screen name="ExhibitionEntered" component={ExhibitionEnteredScreen} options={{headerShown: false}} />
-            <Stack.Screen name="ExhibitionReport" component={ExhibitionReportScreen} options={{headerShown: false}} />
-            <Stack.Screen
-              name="ExhibitionCreation"
-              component={ExhibitionCreationScreen}
-              options={{headerShown: false}}
-            />
-            <Stack.Screen
-              name="ExhibitionFilterApplyAll"
-              component={ExhibitionFilterApplyAllScreen}
-              options={{headerShown: false}}
-            />
-            <Stack.Screen
-              name="ExhibitionFilterApply"
-              component={ExhibitionFilterApplyScreen}
-              options={{headerShown: false}}
-            />
-            <Stack.Screen
-              name="ExhibitionFilterApplyComplete"
-              component={ExhibitionFilterApplyCompleteScreen}
-              options={{headerShown: false}}
-            />
-            <Stack.Screen name="PlacesSearch" component={PlacesSearchScreen} options={{headerShown: false}} />
-          </Stack.Group>
-          <Stack.Group>
-            <Stack.Screen name="FilterCreation" component={FilterCreationScreen} options={{headerShown: false}} />
-            <Stack.Screen
-              name="FilterCreationDesc"
-              component={FilterCreationDescScreen}
-              options={{headerShown: false}}
-            />
-            <Stack.Screen
-              name="FilterCreationGallery"
-              component={GalleryScreen}
-              options={{headerShown: false, presentation: 'modal'}}
-            />
-          </Stack.Group>
-        </Stack.Navigator>
-      </NavigationContainer>
+      <QueryClientProvider client={queryClient}>
+        <NavigationContainer theme={MyTheme}>
+          <Stack.Navigator>
+            <Stack.Group>
+              <Stack.Screen name="Main" component={BottomTab} options={{headerShown: false}} />
+              <Stack.Screen name="Map" component={MapScreen} options={{headerShown: false}} />
+              <Stack.Screen name="HomeSearch" component={HomeSearchScreen} options={{headerShown: false}} />
+            </Stack.Group>
+            <Stack.Group>
+              <Stack.Screen name="Exhibition" component={ExhibitionScreen} options={{headerShown: false}} />
+              <Stack.Screen
+                name="ExhibitionEntered"
+                component={ExhibitionEnteredScreen}
+                options={{headerShown: false}}
+              />
+              <Stack.Screen name="ExhibitionReport" component={ExhibitionReportScreen} options={{headerShown: false}} />
+              <Stack.Screen
+                name="ExhibitionCreation"
+                component={ExhibitionCreationScreen}
+                options={{headerShown: false}}
+              />
+              <Stack.Screen
+                name="ExhibitionFilterApplyAll"
+                component={ExhibitionFilterApplyAllScreen}
+                options={{headerShown: false}}
+              />
+              <Stack.Screen
+                name="ExhibitionFilterApply"
+                component={ExhibitionFilterApplyScreen}
+                options={{headerShown: false}}
+              />
+              <Stack.Screen
+                name="ExhibitionFilterApplyComplete"
+                component={ExhibitionFilterApplyCompleteScreen}
+                options={{headerShown: false}}
+              />
+              <Stack.Screen name="PlacesSearch" component={PlacesSearchScreen} options={{headerShown: false}} />
+            </Stack.Group>
+            <Stack.Group>
+              <Stack.Screen name="FilterCreation" component={FilterCreationScreen} options={{headerShown: false}} />
+              <Stack.Screen
+                name="FilterCreationDesc"
+                component={FilterCreationDescScreen}
+                options={{headerShown: false}}
+              />
+              <Stack.Screen
+                name="FilterCreationGallery"
+                component={GalleryScreen}
+                options={{headerShown: false, presentation: 'modal'}}
+              />
+            </Stack.Group>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </QueryClientProvider>
     </SafeAreaProvider>
   );
 }
