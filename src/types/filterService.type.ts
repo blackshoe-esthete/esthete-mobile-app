519d036b6f1704032a8bbe35452361f387056d53
Kong-E <kng001016@dgu.ac.kr>
2024-07-14 16:27:59 +0900

    [EST-41] Update: 필터서비스 타입 업데이트

--- src/types/filterService.type.ts
+++ src/types/filterService.type.ts
@@ -1,15 +1,15 @@
 export type FilterType =
-  | 'sharpeness'
+  | 'sharpness'
   | 'exposure'
   | 'brightness'
   | 'contrast'
   | 'saturation'
   | 'hue'
   | 'temperature'
-  | 'grayscale';
+  | 'gray_scale';
 
 export type FilterValue = Partial<Record<FilterType, number>> & {
-  grayscale?: number;
+  gray_scale?: number;
 };
 
 // 필터 생성 시 필요한 데이터
@@ -85,4 +85,63 @@ export type Matrix = [
 ];
 
 // 필터 태그
-export type FilterTagType = '따뜻한' | '부드러운' | '평화로운' | '차가운' | '빈티지한' | '몽환적인' | '싱그러운';
+export type FilterTagType =
+  | '따뜻한'
+  | '부드러운'
+  | '평화로운'
+  | '차가운'
+  | '빈티지한'
+  | '몽환적인'
+  | '싱그러운'
+  | '세련된'
+  | '자연스러운'
+  | '클래식한'
+  | '쾌활한'
+  | '우아한'
+  | '잔잔한'
+  | '풍요로운'
+  | '짙은'
+  | '화사한'
+  | '모던한'
+  | '수수한'
+  | '사랑스러운'
+  | '캐주얼한'
+  | '자유분방한'
+  | '다채로운'
+  | '차분한'
+  | '신비로운'
+  | '순수한'
+  | '고요한'
+  | '고급스러운';
+
+// 필터 태그 리스트
+interface FilterAttributes {
+  filter_id: string;
+  brightness: number;
+  sharpness: number;
+  exposure: number;
+  contrast: number;
+  saturation: number;
+  hue: number;
+  temperature: number;
+  gray_scale: number;
+}
+
+// 필터 이미지 리스트
+interface RepresentationImgList {
+  representation_img_list: string[];
+}
+
+//
+interface FilterTagList {
+  filter_tag_list: FilterTagType[];
+}
+
+export interface TemporaryFilter {
+  temporary_filter_id: string;
+  filter_thumbnail: string;
+  filter_attributes: FilterAttributes;
+  representation_img_list: RepresentationImgList;
+  filter_tag_list: FilterTagList;
+  updated_at: string;
+}
