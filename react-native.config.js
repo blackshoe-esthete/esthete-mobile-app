efcb90ed5c25be6c1e66d418504b65fae54f0ba2
Kong-E <kng001016@dgu.ac.kr>
2024-07-20 23:28:40 +0900

    Update: config 수정

--- react-native.config.js
+++ react-native.config.js
@@ -1,6 +1,5 @@
-module.exports = {
-  dependencies: {
-    // Conditionally exclude 'react-native-flipper' for iOS builds if NO_FLIPPER is set
-    ...(process.env.NO_FLIPPER === '1' ? {'react-native-flipper': {platforms: {ios: null}}} : {}),
-  },
+export const dependencies = {
+  // Conditionally exclude 'react-native-flipper' for iOS builds if NO_FLIPPER is set
+  // eslint-disable-next-line no-undef
+  ...(process.env.NO_FLIPPER === '1' ? { 'react-native-flipper': { platforms: { ios: null } } } : {}),
 };
