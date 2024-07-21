6392d01ca69d6e0c4b0815515d2d45ef4b8d0cd5
Kong-E <kng001016@dgu.ac.kr>
2024-07-21 15:48:14 +0900

    Fix: react-native.config.js

--- react-native.config.js
+++ react-native.config.js
@@ -1,5 +1,6 @@
-export const dependencies = {
-  // Conditionally exclude 'react-native-flipper' for iOS builds if NO_FLIPPER is set
-  // eslint-disable-next-line no-undef
-  ...(process.env.NO_FLIPPER === '1' ? { 'react-native-flipper': { platforms: { ios: null } } } : {}),
+module.exports = {
+  dependencies: {
+    // Conditionally exclude 'react-native-flipper' for iOS builds if NO_FLIPPER is set
+    ...(process.env.NO_FLIPPER === '1' ? { 'react-native-flipper': { platforms: { ios: null } } } : {}),
+  },
 };
